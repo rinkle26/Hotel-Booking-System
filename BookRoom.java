2ce35b03637abf61f6fc04125c2b2be15130c2e7
anupjaltade <43056433+anupjaltade@users.noreply.github.com>
2018-10-01 11:28:07 -0500

    Add files via upload

---
+++ BookRoom.java
@@ -0,0 +1,63 @@
+package domain.login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.jdbc.Connection;
+
+import db.DbManager;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/BookRoom")
+public class BookRoom extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public BookRoom() {
+    	DbManager db = new DbManager();
+		Connection conn = (Connection) db.getConnection();
+		if (conn == null)
+			System.out.println("DB connection failed");
+		else
+			System.out.println("DB connection successfull");
+    }
+    
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	//	CustomerDao customerDao = new CustomerDaoImpl();
+		RoomDao roomDao = new RoomDaoImpl();
+		Room r = new Room();
+		String EnteredRoomNumber = request.getParameter("enteredRoomNumber");
+		String submitType = request.getParameter("submit");
+//		Login login = new Login(username, pass);
+//		Customer c = customerDao.validateCustomer(login);
+//		System.out.println("===== "+submitType+" --- "+username+" --- "+pass+" -- "+c.getName()+" ========");
+		
+		if(submitType.equals("View Room")){
+			// print the database
+		}
+//			System.out.println("Login successfull");
+//			request.setAttribute("message", "Hello "+c.getName());
+//			request.getRequestDispatcher("welcome.jsp").forward(request, response);
+		/*}else if(submitType.equals("Register")){
+			c.setName(request.getParameter("name"));
+			c.setUsername(request.getParameter("username"));
+			c.setPassword(request.getParameter("password"));
+			customerDao.register(c);
+			request.setAttribute("successMessage", "Registration done, please login!");
+			request.getRequestDispatcher("login.jsp").forward(request, response);
+		}else{
+			request.setAttribute("message", "Data Not Found! Please register!");
+			request.getRequestDispatcher("register.jsp").forward(request, response);
+		}*/
+
+	}
+
+}
